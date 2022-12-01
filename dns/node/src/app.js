572d006d7b7a7d8abca9b4f1d00251f8127de752
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-12-01 22:35:24 +0900

    commit to learn docker dns

---
+++ dns/node/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+app.get("/", function(req, res, next){
+    res.json([
+      {
+          now: new Date,
+      }
+  ]);
+});
