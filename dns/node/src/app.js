95b79c882f3fd646ac120ec2b46b962fe9bf8e88
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-12-05 20:28:28 +0900

    run format

--- dns/node/src/app.js
+++ dns/node/src/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
 
-const server = app.listen(3000, function(){
+const server = app.listen(3000, function () {
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
-app.get("/", function(req, res, next){
+app.get("/", function (req, res, next) {
     res.json([
-      {
-          now: new Date,
-      }
-  ]);
+        {
+            now: new Date,
+        }
+    ]);
 });
 
-app.get("/redirect", function(req, res, next){
+app.get("/redirect", function (req, res, next) {
     res.redirect(req.headers.host + '/');
 });
