7d4b12cc8ed570236f146550d5e771049bf301e7
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-12-08 20:40:23 +0900

    use arrow func

--- dns/node/src/app.js
+++ dns/node/src/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
-const server = app.listen(3000, function () {
+const server = app.listen(3000, () => {
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
-app.get("/", function (req, res, next) {
+app.get("/", (req, res, next) => {
     res.json([
         {
             now: new Date,
@@ -13,6 +13,6 @@ app.get("/", function (req, res, next) {
     ]);
 });
 
-app.get("/redirect", function (req, res, next) {
+app.get("/redirect", (req, res, next) => {
     res.redirect(req.headers.host + '/');
 });
